02bbdab061de379ada22de8506a973ce59c92df4
Tim Heiszwolf <32781879+TimHeiszwolf@users.noreply.github.com>
2020-05-02 00:27:35 +0200

    ADD makeRandomDatabaseMultiprocessing
    Added a function to generate the database with multiprocessing to increase speed.

--- makeRandomDatabase.py
+++ makeRandomDatabase.py
@@ -4,6 +4,7 @@ import pandas as pd
 import time
 import random
 import math
+from multiprocessing import Process, Queue
 
 from generateSpace import *
 from plotLattice import *
@@ -14,18 +15,17 @@ from potentialEnergyPerParticle import *
 from numberOfCalculations import *
 
 
-def makeRandomDatabase(numberOfDatapoints, numberOfSurroundingCells, numberOfParticlesPerCell, potentialEnergyFunction, widthOfCellRange = [1, 1], filename = False):
+def makeRandomDatabase(numberOfDatapoints, numberOfSurroundingCells, numberOfParticlesPerCell, potentialEnergyFunction, widthOfCellRange = [1, 1], filename = False, giveUpdates = True):
     """
     A function that generates a random database of particle configurations and their potential energies.
     
     """
-    
-    secondsPerDatapoint = 15 * (10**-6) * numberOfCalculations(numberOfSurroundingCells, numberOfParticlesPerCell)
-    print('Predicted amount of calculations:', int(numberOfDatapoints * numberOfCalculations(numberOfSurroundingCells, numberOfParticlesPerCell)))
-    print('Assuming 15 us per calculation, estimated time needed:', math.ceil(numberOfDatapoints * secondsPerDatapoint), 'seconds')
-    # Give the user some information and estimates about the amount of calculation and time needed.
-    
-    startTime = time.time()
+    if giveUpdates:
+        # Give the user some information and estimates about the amount of calculation and time needed.
+        secondsPerDatapoint = 15 * (10**-6) * numberOfCalculations(numberOfSurroundingCells, numberOfParticlesPerCell)
+        print('Predicted amount of calculations:', int(numberOfDatapoints * numberOfCalculations(numberOfSurroundingCells, numberOfParticlesPerCell)), 'for ', numberOfDatapoints, 'datapoints.')
+        print('Assuming 15 us per calculation, estimated time needed:', math.ceil(numberOfDatapoints * secondsPerDatapoint), 'seconds')
+        startTime = time.time()
     
     data = {'particleCoordinates': [],'widthOfCell':[] , 'numberOfSurroundingCells': [], 'potentialEnergy':[]}
     
@@ -39,10 +39,12 @@ def makeRandomDatabase(numberOfDatapoints, numberOfSurroundingCells, numberOfPar
         data['numberOfSurroundingCells'].append(numberOfSurroundingCells)
         data['potentialEnergy'].append(potentialEnergy(otherSpace, particles, potentialEnergyFunction))# Calculate the potential energy and save it to the data dictonairy.
         
-        print(str(math.ceil(100 * (i + 1) / (numberOfDatapoints ))).rjust(3, ' '), '% done, time left', math.ceil((numberOfDatapoints - i - 1) * secondsPerDatapoint), 'seconds,', math.ceil(time.time() - startTime), 'seconds since start.')# Inform the user of the progress.
+        if giveUpdates:
+            print(str(math.ceil(100 * (i + 1) / (numberOfDatapoints ))).rjust(3, ' '), '% done, time left', math.ceil((numberOfDatapoints - i - 1) * secondsPerDatapoint), 'seconds,', math.ceil(time.time() - startTime), 'seconds since start.')# Inform the user of the progress.
     
-    endTime = time.time()
-    print('Done generating database. Took :', math.ceil(endTime - startTime), 'seconds, predicted', math.ceil(numberOfDatapoints * secondsPerDatapoint), 'seconds.')# Inform the user of how the procces has progressed.
+    if giveUpdates:
+        endTime = time.time()
+        print('Done generating database. Took :', math.ceil(endTime - startTime), 'seconds, predicted', math.ceil(numberOfDatapoints * secondsPerDatapoint), 'seconds.')# Inform the user of how the procces has progressed.
     
     dataDF = pd.DataFrame(data)# Turn the data into a pandas DataFrame to make data easier to handle.
     
@@ -52,31 +54,32 @@ def makeRandomDatabase(numberOfDatapoints, numberOfSurroundingCells, numberOfPar
     
     return dataDF
 
-numberOfDatapoints = 1000
-numberOfSurroundingCells = 4
-numberOfParticlesPerCell = 4
-potentialEnergyFunction = potentialEnergyPerTrio# Set the potential energy function of the data base as a function
-widthOfCell = [0.5, 100]# The width of a singe cell.
-filename = 'test2'# Name of the file in which the data will be stored, set to a boolean if you don't want to store the data.
 
-data = makeRandomDatabase(numberOfDatapoints, numberOfSurroundingCells, numberOfParticlesPerCell, potentialEnergyFunction, widthOfCell, filename)
+def makeRandomDatabaseSingleQueue(q, numberOfDatapoints, numberOfSurroundingCells, numberOfParticlesPerCell, potentialEnergyFunction, widthOfCellRange, giveUpdates):
+    q.put(makeRandomDatabase(numberOfDatapoints, numberOfSurroundingCells, numberOfParticlesPerCell, potentialEnergyFunction, widthOfCellRange, False, giveUpdates))
 
-qLow = data['potentialEnergy'].quantile(0)
-qHi  = data['potentialEnergy'].quantile(0.80)
 
-dataFiltered = data[(data['potentialEnergy'] < qHi) & (data['potentialEnergy'] > qLow)]
-
-x = dataFiltered['potentialEnergy']
-num_bins = 100
-
-fig, ax = plt.subplots(figsize=(8, 8))
-#ax.set_xlim([0, 10**19])
-#ax.set_xscale('log')
-n, bins, patches = ax.hist(x, num_bins, facecolor='blue', alpha=0.5)
-plt.show()
-
-amountToPlot = min([10, numberOfDatapoints])
-if input('Want to plot ' + str(amountToPlot) + ' lattices? (Y/N)').lower() == 'y':
-    for i in range(0, amountToPlot):
-        print('Plotting datapoint ', i, ' with energy ', data['potentialEnergy'][i])
-        plotLatticeFromDataFrame(data, i)
+def makeRandomDatabaseMultiprocessing(numberOfDatapoints, numberOfSurroundingCells, numberOfParticlesPerCell, potentialEnergyFunction, widthOfCellRange = [1, 1], filename = False, amountOfProcesses = 4):
+    """
+    A function that impliments multiprocessing for the generation of the database.
+    """
+    #https://stackoverflow.com/questions/1182315/python-multicore-processing#1182350
+    q = Queue()
+    
+    processes = []
+    print('Using multiprocessing only the first procces gives updates')
+    processes.append(Process(target = makeRandomDatabaseSingleQueue, args = (q, math.ceil(numberOfDatapoints / amountOfProcesses), numberOfSurroundingCells, numberOfParticlesPerCell, potentialEnergyFunction, widthOfCellRange, True)))
+    for i in range(0, amountOfProcesses - 1):
+        processes.append(Process(target = makeRandomDatabaseSingleQueue, args = (q, math.ceil(numberOfDatapoints / amountOfProcesses), numberOfSurroundingCells, numberOfParticlesPerCell, potentialEnergyFunction, widthOfCellRange, False)))
+    
+    for i in processes:
+        i.start()
+    time.sleep(5)
+    
+    dataDF =pd.concat([q.get() for i in range(0, amountOfProcesses)], ignore_index=True, sort =False)
+    
+    if type(filename) == str:
+        # If wanted save the data to a json file.
+        dataDF.to_json(filename + '.json', orient='columns')
+    
+    return dataDF
---
+++ runDatabaseGeneration.py
@@ -0,0 +1,86 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib.ticker import (AutoMinorLocator, MultipleLocator)
+import time
+import random
+import math
+from multiprocessing import Process, Queue
+
+from generateSpace import *
+from plotLattice import *
+from getTriangleLengths import *
+from potentialEnergyPerTrio import *
+from potentialEnergy import *
+from potentialEnergyPerParticle import *
+from numberOfCalculations import *
+from makeRandomDatabase import *
+
+
+def main():
+    numberOfDatapoints = 2500
+    numberOfSurroundingCells = 4
+    numberOfParticlesPerCell = 4
+    potentialEnergyFunction = potentialEnergyPerTrio# Set the potential energy function of the data base as a function.
+    widthOfCell = [0.5, 50]# The width of a singe cell.
+    filename = 'Database1_5'# Name of the file in which the data will be stored, set to a boolean if you don't want to store the data.
+    
+    if input('Do you want to use multiprocessing? (Y/N): ').lower()=='y':
+        # Asks the user if multiprocessing is wanted.
+        amountOfProcesses = False
+        while (not (type(amountOfProcesses) == int)):
+            # Keeps asking the user for the amountOfProcesses until it is a int.
+            amountOfProcesses = int(input('How many processes should start? (int): '))
+            
+        print('Starting multiprocessing.')
+        data = makeRandomDatabaseMultiprocessing(numberOfDatapoints, numberOfSurroundingCells, numberOfParticlesPerCell, potentialEnergyFunction, widthOfCell, filename, amountOfProcesses)
+    else:
+        print('Starting singleprocessing.')
+        data = makeRandomDatabase(numberOfDatapoints, numberOfSurroundingCells, numberOfParticlesPerCell, potentialEnergyFunction, widthOfCell, filename)
+    
+    print('\n Done generating database now analysing \n')
+    print(data.describe())
+    print('\n Now plotting historgrams \n')
+    
+    ## The code below is for plotting diagrams.
+    # Filter the data for the logarithmic historgram.
+    qLow = data['potentialEnergy'].quantile(0)
+    qHi  = data['potentialEnergy'].quantile(0.99)
+    dataFiltered = data[(data['potentialEnergy'] < qHi) & (data['potentialEnergy'] > qLow)]
+    x = dataFiltered['potentialEnergy']
+    bins = [5**i for i in range(math.floor(math.log(min(x)) / math.log(5)) - 4, 26)]
+    
+    # Make the logarithmic historgram.
+    fig, ax = plt.subplots(figsize=(8, 8))
+    n, bins, patches = ax.hist(x, bins = bins, facecolor='blue', alpha=0.5)
+    ax.set_xscale('log')
+    ax.set_xlim(max(math.floor(math.log(min(x)) / math.log(10)), 10**-6), math.pow(10, 1 + math.ceil(math.log(max(x)) / math.log(10))))
+    ax.set_title('Histogram (logarithmic) of distribution of energy per particle.')
+    ax.set_xlabel('Energy per particle')
+    ax.set_ylabel('Count')
+    plt.show()
+    
+    # Filter the data for the normal historgram.
+    qLow = data['potentialEnergy'].quantile(0.001)
+    qHi  = data['potentialEnergy'].quantile(0.80)
+    dataFiltered = data[(data['potentialEnergy'] < qHi) & (data['potentialEnergy'] > qLow)]
+    x = dataFiltered['potentialEnergy']
+    numBins = 1000
+    
+    # Make the normal historgram.
+    fig, ax = plt.subplots(figsize=(8, 8))
+    n, bins, patches = ax.hist(x, numBins, facecolor='blue', alpha=0.5)
+    ax.set_title('Histogram of distribution of energy per particle.')
+    ax.set_xlabel('Energy per particle')
+    ax.set_ylabel('Count')
+    plt.show()
+
+    amountToPlot = min([10, numberOfDatapoints])
+    if input('Want to plot ' + str(amountToPlot) + ' lattices? (Y/N): ').lower() == 'y':
+        # Plotting the lattices if wanted.
+        for i in range(0, amountToPlot):
+            print('Plotting datapoint ', i, ' with energy ', data['potentialEnergy'][i])
+            plotLatticeFromDataFrame(data, i)
+
+if __name__=='__main__':
+    main()
